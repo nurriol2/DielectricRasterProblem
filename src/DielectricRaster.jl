f8b8ce7a8b5af8eac735344901fe83c5a5eacdc4
nurriol2 <niko.urriola1@gmail.com>
2022-08-16 01:31:17 -0500

    Add module for pixel getters

--- src/DielectricRaster.jl
+++ src/DielectricRaster.jl
@@ -17,6 +17,43 @@ end
 
 const TensorGriddedArray = GriddedArray{SMatrix{2, 2, Float64}}
 
+module PixelOperations
+#=
+    This module provides getters for  pixel characteristics, such as height
+    and area.
+
+    These pixels are defined in `GriddedArray.pixels`
+=#
+
+function pixel_width(A::GriddedArray)
+    ncols = size(A.pixels, 2)
+    (A.xlims[2] - A.xlims[1]) / (ncols - 1)
+
+end
+
+function pixel_height(A::GriddedArray)
+    nrows = size(A.pixels, 1)
+    (A.ylims[2] - A.ylims[1]) / (nrows-1)
+end
+
+function pixel_center(A::GriddedArray, index::Int64) 
+
+    nrows = size(A.pixels, 1)
+    ncols = size(A.pixels, 2)
+    
+    Δx = pixel_width(A)
+    Δy = pixel_height(A)
+    
+    xcenter = A.xlims[1] + Δx * ((index - 1.0) % ncols) 
+    ycenter = A.ylims[1] + Δy * ((index - 1.0) ÷ ncols)
+    
+    (xcenter, ycenter)
+end
+
+total_pixel_area(A::GriddedArray) = pixel_width(A) * pixel_height(A)
+
+end # PixelOperations
+
 ## Q1
 function raster_area!(A::GriddedArray, s::Square)
     # implement me
