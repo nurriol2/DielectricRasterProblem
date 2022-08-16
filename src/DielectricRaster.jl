40a2bb19f447f012bf3543f827b1f355bb5e0a86
nurriol2 <niko.urriola1@gmail.com>
2022-08-16 01:52:38 -0500

    Add rectangle abstraction for pixels and `Square`s
    The algorithm for finding the intersectional area between rectangles
    is well known. So, this modlue was added to provide an interface
    that represents pixels and `Square`s as generic rectangles.
    
    Then, the solution to this problem can be rephrased as
    
    "Calculate the ratio of intersectional area to total pixel area"

--- src/DielectricRaster.jl
+++ src/DielectricRaster.jl
@@ -25,6 +25,7 @@ module PixelOperations
     These pixels are defined in `GriddedArray.pixels`
 =#
 
+
 function pixel_width(A::GriddedArray)
     ncols = size(A.pixels, 2)
     (A.xlims[2] - A.xlims[1]) / (ncols - 1)
@@ -53,6 +54,79 @@ end
 total_pixel_area(A::GriddedArray) = pixel_width(A) * pixel_height(A)
 
 end # PixelOperations
+
+module RectangleAbstraction 
+
+#=
+    This module provides methods that are useful for representing pixels and
+    Squares as generic rectangles. 
+
+    In addition, an algorithm is implemented to find the intersectional area
+    between two generic rectangles. 
+=# 
+
+
+#=
+    Compute the lines forming the boundary of a (Square) rectangle from a Square
+
+    Ex: Top surface -> y = (Square center y coordinate) + (1/2 Square width)
+=#
+right_edge(s::Square) = s.x + s.w/2
+left_edge(s::Square) = s.x - s.w/2
+top_edge(s::Square) = s.y + s.w/2
+bot_edge(s::Square) = s.y - s.w/2
+
+# Rectangle representation for the pixels found in `GriddedArray.pixels`
+function right_edge(A::GriddedArray, index::Int64)
+    # Find x coord pixel center
+    xcenter, _ = pixel_center(A, index)
+    # Find pixel width
+    w = pixel_width(A)
+    # Compute edge loc
+    xcenter + w/2
+end
+
+function left_edge(A::GriddedArray, index::Int64)
+    # Find x coord pixel center
+    xcenter, _ = pixel_center(A, index)
+    # Find pixel width
+    w = pixel_width(A)
+    # Compute edge loc
+    xcenter - w/2
+end
+
+function top_edge(A::GriddedArray, index::Int64)
+    # Find y coord pixel center
+    _, ycenter = pixel_center(A, index)
+    # Find pixel height
+    h = pixel_height(A)
+    # Compute edge loc
+    ycenter + h/2
+end
+
+function bot_edge(A::GriddedArray, index::Int64)
+    # Find y coord pixel center
+    _, ycenter = pixel_center(A, index)
+    # Find pixel height
+    h = pixel_height(A)
+    # Compute edge loc
+    ycenter - h/2
+end
+
+# Algorithm for calculating intersectional area between generic rectangles
+function w_prime(A::GriddedArray, index::Int64, s::Square)
+   min(right_edge(s), right_edge(A, index)) - max(left_edge(s), left_edge(A, index)) 
+end
+
+function h_prime(A::GriddedArray, index::Int64, s::Square)
+   min(top_edge(s), top_edge(A, index)) - max(bot_edge(s), bot_edge(A, index))
+end
+
+function overlapping_area(A::GriddedArray, index::Int64, s::Square)
+    max(0, w_prime(A, index, s)) * max(0, h_prime(A, index, s))
+end
+
+end # RectangleAbstraction
 
 ## Q1
 function raster_area!(A::GriddedArray, s::Square)
